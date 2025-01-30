71c8a887c3656c0346638ccfb6b29387511f9eb3
BhetwalAyush <rujanbhetwal@gmail.com>
2025-01-31 01:16:34 +0545

    updating_api_for user_profileImage

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,16 @@
 
-const router = require('express').Router()
-
-const express = require('express');
 // const nodemailer = require('nodemailer');
 const User = require('../models/userModels');
+const express = require('express');
+const router = express.Router();
+const upload = require("../middleware/uploads").default; 
 const userControllers = require('../controllers/userController');
 // Make a create user API
 router.post('/create', userControllers.createUser);
 
 // Login user API
 router.post('/login', userControllers.loginUser)
+router.post("/uploadImage", upload, userControllers.uploadImage);  
 
 
 module.exports = router;
